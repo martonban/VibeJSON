97acecb289fdcd5e35384e2458828746488dd40e
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-04-11 02:25:29 +0200

    ImGUI Issues

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,21 +1,22 @@
-#include "raylib.h"
-#include "raymath.h"
-
 #include <iostream>
 #include <string>
+
+#include "raylib.h"
+#include "raymath.h"
 
 #include "imgui.h"
 #include "rlImGui.h"
 
-/*-------------------------------------------------------------------------------------------------------------------------------
-*                                          	NebulaPax - SpaceOS
-*                                      	Copyright (c) Márton Bán 2025
-*	SpaceOS is powered by the NebulaPax engine. This engine evolves iteratively, with each new game representing a new
-*	version of the engine, continuously improving and expanding its capabilities.
-*-------------------------------------------------------------------------------------------------------------------------------
-*/
+
+//-------------------------------------------------------------------------------------------------------------------------------*                                          	
+//												NebulaPax - SpaceOS
+//                                      	Copyright (c) Márton Bán 2025
+//	SpaceOS is powered by the NebulaPax engine. This engine evolves iteratively, with each new game representing a new
+//	version of the engine, continuously improving and expanding its capabilities.
+//-------------------------------------------------------------------------------------------------------------------------------
 
 // DPI scaling functions
+
 float ScaleToDPIF(float value) {
     return GetWindowScaleDPI().x * value;
 }
@@ -36,12 +37,12 @@ void rendering(Camera3D& camera, RenderTexture2D& target) {
 
 
 int main(int argc, char* argv[]) {
-	int screenWidth = 1280;
-	int screenHeight = 800;
+	int SCREEN_WIDTH = 1280;
+	int SCREEN_HEIGHT = 800;
 
 	SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
-	InitWindow(screenWidth, screenHeight, "raylib-Extras [ImGui] example - simple ImGui Demo");
-	SetTargetFPS(144);
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib-Extras [ImGui] example - simple ImGui Demo");
+	SetTargetFPS(60);
 	rlImGuiSetup(true);
 
 	Camera3D camera = { 0 };
@@ -50,15 +51,14 @@ int main(int argc, char* argv[]) {
 	camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
 	camera.fovy = 45.0f;                         
 	camera.projection = CAMERA_PERSPECTIVE;
-	EndTextureMode();
-    RenderTexture2D frame_renderer = LoadRenderTexture(screenWidth, screenHeight);
+
+    RenderTexture2D frame_renderer = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
 	float f = 0.f;
 	
 
 	while (!WindowShouldClose()) {
-		rendering(camera, frame_renderer);
-
 		BeginDrawing();
+		rendering(camera, frame_renderer);
         DrawTextureRec(frame_renderer.texture, Rectangle{0, 0, (float)frame_renderer.texture.width, -(float)frame_renderer.texture.height}, Vector2{0, 0}, WHITE);
 		
 		// start ImGui Conent
@@ -75,16 +75,16 @@ int main(int argc, char* argv[]) {
 		{
 			ImGui::Text("Hello, world %d", 123);
 			if (ImGui::Button("Save")) {
-				std::cout << "pénisz";
+				std::cout << "Test";
 			}
 			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
 		}
 		ImGui::End();
-		std::cout << std::endl << f << std::endl;
 
 		// end ImGui Content
 		rlImGuiEnd();
 		EndDrawing();
+		std::cout << GetFPS() << std::endl;
 	}
 	UnloadRenderTexture(frame_renderer);
 	rlImGuiShutdown();
